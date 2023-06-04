ce0549209f1ff2f5aa33493e89b23291af4b172c
Gatitulina Julia <J.Gatitulina@trkua.tv>
2023-06-05 02:34:11 +0300

    lesson_30_31_32update

--- Yuliya Gatitulina/Lesson_32/Task_2_client.py
+++ Yuliya Gatitulina/Lesson_32/Task_2_client.py
@@ -1,25 +1,12 @@
 import socket
 
-def caesar_encrypt(text, key):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            ascii_offset = ord('a') if char.islower() else ord('A')
-            encrypted_char = chr((ord(char) - ascii_offset + key) % 26 + ascii_offset)
-            encrypted_text += encrypted_char
-        else:
-            encrypted_text += char
-    return encrypted_text
-
 HOST = "127.0.0.1"
 PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    key = int(input('key: '))
-    text = input('text: ')
-    encrypted_text = caesar_encrypt(text, key)
-    s.sendall(encrypted_text.encode())
+    key = input('key: ')
+    s.sendall(key.encode())
     data = s.recv(1024)
 
 print(f"Received {data!r}")
--- Yuliya Gatitulina/Lesson_32/Task_2_server.py
+++ Yuliya Gatitulina/Lesson_32/Task_2_server.py
@@ -1,5 +1,16 @@
 import socket
 
+def caesar_encrypt(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            ascii_offset = ord('a') if char.islower() else ord('A')
+            encrypted_char = chr((ord(char) - ascii_offset + key) % 26 + ascii_offset)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+    return encrypted_text
+
 HOST = "127.0.0.1"
 PORT = 65432
 
@@ -13,5 +24,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            encrypted_text = caesar_encrypt('Hello!', int(data))
+            print(encrypted_text)
+            conn.sendall(encrypted_text.encode())
+
+
 
