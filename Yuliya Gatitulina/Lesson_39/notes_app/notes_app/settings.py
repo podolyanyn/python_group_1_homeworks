3db84840ebc01b2c925129930fbccc93d29e07d6
Gatitulina Julia <J.Gatitulina@trkua.tv>
2023-06-23 21:41:09 +0300

    lesson_39

--- Yuliya Gatitulina/Lesson_39/notes_app/notes_app/settings.py
+++ Yuliya Gatitulina/Lesson_39/notes_app/notes_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'notes_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'notes/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../notes/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
